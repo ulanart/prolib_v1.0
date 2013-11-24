f355eab05d2bd851149a0411eb5122a84acb34e0
Artjoms "maestro" Ulanchiks <ulanart1992@gmail.com>
2013-11-24 16:51:27 +0200

    Ready To Code

--- src/main/java/lv/revolt/Index.java
+++ src/main/java/lv/revolt/Index.java
@@ -1,18 +1,16 @@
 package lv.revolt;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class Index extends WebPage {
+	Form form = new Form("form");
 	private static final long serialVersionUID = 1L;
-
+	
 	public Index(final PageParameters parameters) {
 		super(parameters);
-
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
-    }
+		add(new Label("message","Welcome!"));
+	}
 }
---
+++ src/main/java/lv/revolt/signup.java
@@ -0,0 +1,31 @@
+package lv.revolt;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class signup extends WebPage
+{
+	private TextField email;
+	private TextField login;
+	private TextField pass;
+	private Button signup;
+	public signup(final PageParameters parameters)
+	{
+		super(parameters);
+		Form form = new Form("form");
+		form.add(new Button("signup")
+		{
+			@Override
+			public void onSubmit()
+			{
+				String value = (String)email.getModelObject();
+				String value1 = (String)login.getModelObject();
+				String value2 = (String)pass.getModelObject();
+			}
+		});
+		add(form);
+	}
+}
